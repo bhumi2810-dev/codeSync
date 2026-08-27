1509fdf0ad0b9c0ce655a7f8ee3109adcfc46f32
Bhumi Bhumi <bbhumi576@gmail.com>
2026-08-27 10:46:53 +0530

    changes in prisma

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -3,14 +3,23 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { env } from "../config/env";
 
 declare global {
-  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
-}
+}// telling typescript that prisma ka varName either prismaClient or undefined ho skta hai 
+// basically multiple ek naam k na bane 
 
 const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+// PostgreSQL adapter create kar rahe hain
+// DATABASE_URL ke through PostgreSQL se connection establish hoga
 
 export const prisma = global.prisma || new PrismaClient({ adapter });
+// Agar global PrismaClient already exist karta hai → wahi use karo
+// Agar nahi hai → naya PrismaClient create karo
+// prisma ko export kar rahe hain taaki service files ise use kar saken
 
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
 }
+
+// Check kar rahe hain ki application production mein nahi chal rahi
+// Development mein Prisma instance ko global variable mein store kar rahe hain
+  // Isse baar-baar naye PrismaClient instances create nahi hote
